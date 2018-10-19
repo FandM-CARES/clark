6807e9ee14176a9246dd31f92d363d1b6ba4b272
Joe Cummings <jrcummings27@gmail.com>
2018-10-19 11:46:45 -0500

    initial commit

---
+++ processed-data.py
@@ -0,0 +1,12 @@
+import csv
+
+class ProcessedData:
+
+    def __init__(self, linkToRawDataSet):
+        self.processedData = self.processDataUnigrams(linkToRawDataSet)
+
+    def processDataUnigrams(self, linkToRawDataset):
+        
+        def parseCSVIntoUnigrams(self, linkToRawDataset):
+            
+
