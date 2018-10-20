3e5bb48c0d7342a60f04c5d2e4b9f54816d742b7
Joe Cummings <jrcummings27@gmail.com>
2018-10-20 17:35:33 -0500

    parsing csv data into unigrams and removing stopwords

--- model.py
+++ model.py
@@ -3,6 +3,6 @@ class Model:
     def __init__(self, datasets=[]):
         self.datasets = datasets
     
-    def train(self, self.datasets):
-        
+    # def train(self, self.datasets):
+
     
---
+++ test.py
@@ -0,0 +1,5 @@
+from model import *
+from processedData import *
+import re
+
+processedData = ProcessedData('./data/game1_big_trial.csv')
