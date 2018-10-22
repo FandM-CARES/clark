711d9c62148b2e4e8d60a6fccdb6c6292481199f
Joe Cummings <jrcummings27@gmail.com>
2018-10-21 23:33:39 -0500

    splitting AV for bigrams into independent bayes classifications

--- processedData.py
+++ processedData.py
@@ -7,29 +7,52 @@ stop_words = set(stopwords.words('english'))
 
 class ProcessedData(object):
 
-    appraisalVariables = ['Pleasantness', 'Attention', 'Control',
-                          'Certainty', 'Anticipated Effort', 'Responsibililty']
-
-    def __init__(self, linkToRawDataSet):
+    def __init__(self, linkToRawDataSet, variables):
         self.input = linkToRawDataSet
-        self.result = self.processDataUnigrams(linkToRawDataSet)
+        self.result = self.processDataUnigrams(linkToRawDataSet, variables)
 
-    def processDataUnigrams(self, linkToRawDataset):
-        print(self.parseCSVIntoLabelledUnigrams(linkToRawDataset))
+    def processDataUnigrams(self, linkToRawDataset, variables):
+        result = {}
+        for var in variables:
+            result[var] = self.normalizeValues(self.parseCSVIntoLabelledUnigrams(linkToRawDataset, var))
+        return result
 
-    def parseCSVIntoLabelledUnigrams(self, linkToRawDataset, words={}):
+    def parseCSVIntoLabelledUnigrams(self, linkToRawDataset, variable, words={}):
         with open(linkToRawDataset, mode='r') as rawData:
             csv_reader = csv.DictReader(rawData)
             for row in csv_reader:
+                weight = self.determineWeight(row, variable)
                 for word in re.sub(r'[.!,;?]', " ",  row['Player Message']).split(): # substituting any non-alphanumerical with a space and then splitting on it to get words
                     if word in stop_words:
                         continue
                     word = word.lower()
                     if word in words:
-                        for variable in self.appraisalVariables:
-                            words[word][variable] += float(row[variable])
+                        words[word][weight] += 1
                     else:
-                        words[word] = {}
-                        for variable in self.appraisalVariables:
-                            words[word][variable] = float(row[variable])
-        return words, len(words)
+                        words[word] =  {'low': 0, 'med': 0, 'high': 0} 
+                        words[word][weight] = 1
+                        
+        return words 
+
+    def determineWeight(self, row, variable):
+        if float(row[variable]) < 1.0: 
+            return 'low'
+        elif float(row[variable]) < 2.0:
+            return 'med'
+        else: 
+            return 'high'
+    
+    def normalizeValues(labelledUnigrams):
+        for word in labelledUnigrams:
+            if word['low'] == 0:
+                word['low'] = float(1)/(len(variable_low)+1)
+            else:
+                word['low'] = float(word['low'])/len(variable_low)
+            if word['med'] == 0:
+                word['med'] = float(1)/(len(variable_med)+1)
+            else:
+                word['med'] = float(word['med'])/len(variable_med)
+            if word['high'] == 0:
+                word['high'] = float(1)/(len(variable_high)+1)
+            else:
+                word['high'] = float(word['high'])/len(variable_high)
--- test.py
+++ test.py
@@ -2,4 +2,8 @@ from model import *
 from processedData import *
 import re
 
-processedData = ProcessedData('./data/game1_big_trial.csv')
+appraisalVariables = ['Pleasantness', 'Attention', 'Control',
+                          'Certainty', 'Anticipated Effort', 'Responsibililty']
+
+pd = ProcessedData('./data/game1_big_trial.csv', appraisalVariables)
+print(pd.result)
