1d24cc52f4499b8d75c427e20fb93bb84641f63b
Joe Cummings <jrcummings27@gmail.com>
2018-11-14 22:02:14 -0600

    confusion matrices

--- test.py
+++ test.py
@@ -17,27 +17,23 @@ def n_fold_test(data_list, n):
     np.random.shuffle(np_data)
     splits = np.array_split(np_data, n)
 
-    mean_accuracies = {}
     mean_fscores = {}
     for var in Model().variables:
-        mean_accuracies[var] = 0
         mean_fscores[var] = 0
 
     for i, split in enumerate(splits):
         clark = Model()
         clark.train(np.concatenate(splits[:i]+splits[i+1:]))
         clark.test(splits[i])
-        for var in clark.accuracies:
-            mean_fscores[var] += clark.fscores[var]
-            mean_accuracies[var] += clark.accuracies[var]
+        for var in clark.precisions:
+            mean_fscores[var] += clark.fscores[var]    
 
-    for var in mean_accuracies:
+    for var in mean_fscores:
         mean_fscores[var] /= n
-        mean_accuracies[var] /= n
 
     return mean_fscores
 
-def train_test_split(data_list, s):
+def train_test_split(data_list, s, show_matrix=False):
     """
     Performs a single train-test split test
 
@@ -48,10 +44,15 @@ def train_test_split(data_list, s):
     Object: FScore results for each AV
     """
     data = ProcessedData(data_list)
+    np_data = np.asarray(data.data)
+    np.random.shuffle(np_data)
     training_data, testing_data = data.split_data([data.data], s)
 
     clark = Model()
     clark.train(training_data)
     clark.test(testing_data)
+
+    if show_matrix:
+        clark.confusion_matrix()
     
-    return clark.fscores
+    return clark.precisions
