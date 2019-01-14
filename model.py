83823b9a6727ff753c5c3f24f43acddad49b2784
Joe Cummings <jrcummings27@gmail.com>
2019-01-14 17:09:53 -0600

    remove duplications

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import itertools
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from nlp_helpers import *
+from graphing_helpers import *
 
 class EmotionModel(object):
     
@@ -176,38 +177,7 @@ class EmotionModel(object):
         """
 
         cn_matrix = confusion_matrix(self.true, self.pred)
-        self.plot_confusion_matrix(cn_matrix, self.emotions, 'Emotions', normalize)
-
-    def plot_confusion_matrix(self, cm, classes, title, normalize=False, cmap=plt.cm.Blues):
-        """
-        This function prints and plots the confusion matrix.
-        Normalization can be applied by setting `normalize=True`.
-
-        Courtesy of scikit-learn
-        """
-        if normalize:
-            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-        plt.imshow(cm, interpolation='nearest', cmap=cmap)
-        plt.title(title)
-        plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
-
-        fmt = '.2f' if normalize else 'd'
-        thresh = cm.max() / 2.
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, format(cm[i, j], fmt),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
-        plt.tight_layout()
-        plt.savefig('results/confusion_matrices/' + title + '.png')
-        plt.close()
-
+        plot_confusion_matrix(cn_matrix, self.emotions, 'Emotions', normalize)
 
 class ClarkModel(object):
 
@@ -549,35 +519,4 @@ class ClarkModel(object):
 
         for var in self.variables:
             cn_matrix = confusion_matrix(self.true[var], self.pred[var])
-            self.plot_confusion_matrix(
-                cn_matrix, ['low', 'med', 'high'], var, normalize)
-
-    def plot_confusion_matrix(self, cm, classes, title, normalize=False, cmap=plt.cm.Blues):
-        """
-        This function prints and plots the confusion matrix.
-        Normalization can be applied by setting `normalize=True`.
-
-        Courtesy of scikit-learn
-        """
-        if normalize:
-            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-        plt.imshow(cm, interpolation='nearest', cmap=cmap)
-        plt.title(title)
-        plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
-
-        fmt = '.2f' if normalize else 'd'
-        thresh = cm.max() / 2.
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, format(cm[i, j], fmt),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
-        plt.tight_layout()
-        plt.savefig('results/confusion_matrices/' + title + '.png')
-        plt.close()
+            plot_confusion_matrix(cn_matrix, ['low', 'med', 'high'], var, normalize)
