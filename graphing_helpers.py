89fa574c0f2b8b6195df47d97e517f0c819cb3f3
Joe Cummings <jrcummings27@gmail.com>
2020-04-14 20:53:00 -0400

    Re-index files to remove results/ and data/ from git

--- graphing_helpers.py
+++ graphing_helpers.py
@@ -3,19 +3,34 @@ import itertools
 import statistics
 import matplotlib.pyplot as plt
 
+
 def plot_ellsworth_figs(data):
 
-    emotions = ["sadness", "joy", "fear", "anger", "challenge", "boredom", "frustration"]
-    vars = ["pleasantness", "attention", "control",
-                          "certainty", "anticipated_effort", "responsibility"]
+    emotions = [
+        "sadness",
+        "joy",
+        "fear",
+        "anger",
+        "challenge",
+        "boredom",
+        "frustration",
+    ]
+    vars = [
+        "pleasantness",
+        "attention",
+        "control",
+        "certainty",
+        "anticipated_effort",
+        "responsibility",
+    ]
 
     em_plot = {
-        "pleasantness":{x:[] for x in emotions},
-        "attention":{x:[] for x in emotions},
-        "control":{x:[] for x in emotions},
-        "certainty":{x:[] for x in emotions},
-        "anticipated_effort":{x:[] for x in emotions},
-        "responsibility":{x:[] for x in emotions}
+        "pleasantness": {x: [] for x in emotions},
+        "attention": {x: [] for x in emotions},
+        "control": {x: [] for x in emotions},
+        "certainty": {x: [] for x in emotions},
+        "anticipated_effort": {x: [] for x in emotions},
+        "responsibility": {x: [] for x in emotions},
     }
 
     for x in data:
@@ -23,25 +38,42 @@ def plot_ellsworth_figs(data):
             emotion = x[turn]["emotion"]
             for var in vars:
                 em_plot[var][emotion].append(x[turn]["appraisals"][var])
-    
+
     for var in em_plot:
         for em in em_plot[var]:
             em_plot[var][em] = statistics.mode(em_plot[var][em])
 
     fig, ax = plt.subplots()
-    ax.scatter(list(em_plot["pleasantness"].values()), list(em_plot["anticipated_effort"].values()))
+    ax.scatter(
+        list(em_plot["pleasantness"].values()),
+        list(em_plot["anticipated_effort"].values()),
+    )
     ax.set_xlabel("Pleasantness")
     ax.set_ylabel("Effort")
     for i, txt in enumerate(emotions):
-        ax.annotate(txt, ( list(em_plot["pleasantness"].values())[i], list(em_plot["anticipated_effort"].values())[i]))
+        ax.annotate(
+            txt,
+            (
+                list(em_plot["pleasantness"].values())[i],
+                list(em_plot["anticipated_effort"].values())[i],
+            ),
+        )
     plt.savefig("results/figs/(mode)effort_vs_pleasantness.png")
 
     fig, ax = plt.subplots()
-    ax.scatter(list(em_plot["responsibility"].values()), list(em_plot["control"].values()))
+    ax.scatter(
+        list(em_plot["responsibility"].values()), list(em_plot["control"].values())
+    )
     ax.set_xlabel("Responsibility")
     ax.set_ylabel("Control")
     for i, txt in enumerate(emotions):
-        ax.annotate(txt, ( list(em_plot["responsibility"].values())[i], list(em_plot["control"].values())[i]))
+        ax.annotate(
+            txt,
+            (
+                list(em_plot["responsibility"].values())[i],
+                list(em_plot["control"].values())[i],
+            ),
+        )
     plt.savefig("results/figs/(mode)control_vs_res.png")
 
     fig, ax = plt.subplots()
@@ -49,7 +81,13 @@ def plot_ellsworth_figs(data):
     ax.set_xlabel("Attention")
     ax.set_ylabel("Certainty")
     for i, txt in enumerate(emotions):
-        ax.annotate(txt, ( list(em_plot["attention"].values())[i], list(em_plot["certainty"].values())[i]))
+        ax.annotate(
+            txt,
+            (
+                list(em_plot["attention"].values())[i],
+                list(em_plot["certainty"].values())[i],
+            ),
+        )
     plt.savefig("results/figs/(mode)certainty_vs_attention.png")
 
 
@@ -71,11 +109,15 @@ def plot_confusion_matrix(cm, classes, title, normalize=False, cmap=plt.cm.Blues
     plt.yticks(tick_marks, classes)
 
     fmt = ".2f" if normalize else "d"
-    thresh = cm.max() / 2.
+    thresh = cm.max() / 2.0
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                    horizontalalignment="center",
-                    color="white" if cm[i, j] > thresh else "black")
+        plt.text(
+            j,
+            i,
+            format(cm[i, j], fmt),
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black",
+        )
 
     plt.ylabel("True label")
     plt.xlabel("Predicted label")
