62279e51fd64237621300d525e5da2f3e0b912fa
Joe Cummings <jrcummings27@gmail.com>
2020-04-14 20:54:30 -0400

    Correct typo in validation print statement

--- utils/validation.py
+++ utils/validation.py
@@ -10,4 +10,4 @@ def check_X_y_same_size(X: List[List[int]], y: List[int]) -> None:
     print("Not overriden")
     if X.shape[1] != y.shape[0]:
         raise ValueError(
-            f"Differening number of samples provided: X has {X.shape[1]} samples and y has {y.shape[0]} samples")
+            f"Differing number of samples provided: X has {X.shape[1]} samples and y has {y.shape[0]} samples")
